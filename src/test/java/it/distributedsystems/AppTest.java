6d79e35ef50f391fccfc9cf085486700e8453cc6
alibb-polimi-dev <162186641+alibb-polimi-dev@users.noreply.github.com>
2025-02-12 16:24:00 +0100

    Initial Setup

---
+++ src/test/java/it/distributedsystems/AppTest.java
@@ -0,0 +1,20 @@
+package it.distributedsystems;
+
+import org.junit.jupiter.api.*;
+
+import static junit.framework.Assert.*;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigourous Test :-)
+     */
+    @Test
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
